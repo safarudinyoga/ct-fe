d62545f8947cacf5fc017234981361b49c054bc5
safarudinyoga <safarudinyoga@gmail.com>
2022-10-04 11:21:45 +0700

    :zap: add ui dashboard

--- src/pages/DashboardUser/SmartProfile/index.tsx
+++ src/pages/DashboardUser/SmartProfile/index.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import LayoutDashboard from '../Layout';
 
+import '../Layout/layoutdashboard.sass'
+
 interface ISmartProfileProps {
 }
 
 const SmartProfile: React.FunctionComponent<ISmartProfileProps> = (props) => {
   return (
     <LayoutDashboard activeKey='smart-profile'>
-      <div></div>
+      <div class>
+
+      </div>
     </LayoutDashboard>
   );
 };
